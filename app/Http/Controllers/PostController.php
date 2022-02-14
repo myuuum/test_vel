99bff15b276480a247bd43ba99e659e4f055f3c0
myuuum <yuu20001229@gmail.com>
2022-02-14 23:17:13 +0900

    8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
-class PostController extends Controller{
-    public function index(Post $post)
-  {
-    return $post->get();
+class PostController extends Controller
+{
+  public function index(Post $post){
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+  }
+  
+  public function show(Post $post){
+    return view('show')->with(['post' => $post]);
+  }
+  
+  public function create(){
+    return view('create');
+  }
+  
+  public function store(Post $post, PostRequest $request ){
+    $input = $request['post'];
+    $post->create($input)->save();
+    return redirect('/posts/' . $post->id);
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
 
-Route::get('/posts', 'PostController@index');
+Route::post('/posts', 'PostController@store');
